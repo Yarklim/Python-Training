b96060dbd0447f5320a3e0193eeedb8ee05ecafb
Yaroslav Klimas <yarklim@gmail.com>
2026-04-27 15:46:13 +0300

    ADD: 9-cycles

---
+++ Study/1-Base/9-cycles.py
@@ -0,0 +1,17 @@
+# =============== FOR ===============
+for i in range(3):
+    print(i, "Hello")
+
+# ---- от и до ----
+for i in range(2, 5):
+    print(i, "Hello")
+
+# ---- от и до с шагом ----
+for i in range(1, 7, 2):
+    print(i, "Hello")
+
+# ============== WHILE ==============
+count = 0
+while count < 5:
+    print(count, "Hello")
+    count += 1

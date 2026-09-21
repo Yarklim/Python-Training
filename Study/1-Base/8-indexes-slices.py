85c095e64ed4d490561de4a25455414c78de3e18
Yaroslav Klimas <yarklim@gmail.com>
2026-09-21 12:42:50 +0300

    UPDATE: 8-indexes-slices.py

--- Study/1-Base/8-indexes-slices.py
+++ Study/1-Base/8-indexes-slices.py
@@ -17,8 +17,14 @@ negative_slice = url[-4:]  # .com
 reverse_str = url[::-1]  # moc.elgoog.www//:sptth
 
 
-str_2 = "C1, C2, C3, C4, C5"
+str_2 = "C1, C2, C3, C4, C5, C6, C7"
 some_str = str_2[::2]  # C,C,C,C,C
 numbers_str = str_2[1::4]  # 12345
+
+mask = "i_t_s_ _a_ _s_e_c_r_e_t_ _w_o_r_d"
+
+real = mask[::2]
+
+print(real)
 
 print(reverse_str)

e80b2dd24359c89092161653c2ff8f17f2212e96
Yaroslav Klimas <yarklim@gmail.com>
2026-09-26 11:55:52 +0300

    fix: 11-2-tuple-methods.py

--- Study/1-Base/11-2-tuple-methods.py
+++ Study/1-Base/11-2-tuple-methods.py
@@ -4,6 +4,9 @@ data = ('Home', 'Catalog', 'Payment & Shipping', 'About')
 
 apartments = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
 
+print(apartments[1::2])  # (2, 4, 6, 8, 10)
+print(data[1])  # 'Catalog'
+
 vertabrae = (
     'C1',
     'C2',
@@ -39,3 +42,63 @@ print(
     str(vertabrae)
 )  # Строка '('C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12')'
 print(s)  # 'C1-C2-C3-C4-C5-C6-C7-T1-T2-T3-T4-T5-T6-T7-T8-T9-T10-T11-T12'
+
+# ------------------------------------------------------------
+temps = (12, 15, 14, 10, 9, 11, 13)  # кортежи одинаковой длины.
+week = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+
+# Код тут.
+i = 0
+
+while i < len(temps):
+    print(f'{week[i]}: {temps[i]} °C')
+
+    i += 1
+
+# -----------------------------------------------------------
+month = (
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+)
+
+week_days = (
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+)
+first = 'Wednesday'  # День недели первого числа.
+
+first_index = week_days.index(first)
